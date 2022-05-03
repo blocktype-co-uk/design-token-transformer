e1bd8b0f9af5fe23752fc8983ad7b19b3ddac17e
bram2000 <bram2000@users.noreply.github.com>
2022-05-03 18:07:08 +0000

    Tokens updated

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,18 +4,42 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Tue, 03 May 2022 18:07:07 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
-    public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.10196078431372549)
-    public static let colorsRefBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1)
-    public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
-    public static let darkBackground = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let lightBackground = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let amber100 = UIColor(red: 0.941, green: 0.875, blue: 0.792, alpha: 1)
+    public static let amber300 = UIColor(red: 0.863, green: 0.702, blue: 0.510, alpha: 1)
+    public static let amber500 = UIColor(red: 0.804, green: 0.576, blue: 0.302, alpha: 1)
+    public static let amber700 = UIColor(red: 0.757, green: 0.502, blue: 0.239, alpha: 1)
+    public static let amber900 = UIColor(red: 0.682, green: 0.392, blue: 0.145, alpha: 1)
+    public static let burgundy100 = UIColor(red: 0.839, green: 0.776, blue: 0.792, alpha: 1)
+    public static let burgundy300 = UIColor(red: 0.627, green: 0.478, blue: 0.510, alpha: 1)
+    public static let burgundy500 = UIColor(red: 0.467, green: 0.255, blue: 0.302, alpha: 1)
+    public static let burgundy700 = UIColor(red: 0.392, green: 0.196, blue: 0.239, alpha: 1)
+    public static let burgundy900 = UIColor(red: 0.278, green: 0.110, blue: 0.145, alpha: 1)
+    public static let navy100 = UIColor(red: 0.757, green: 0.765, blue: 0.784, alpha: 1)
+    public static let navy300 = UIColor(red: 0.431, green: 0.455, blue: 0.502, alpha: 1)
+    public static let navy500 = UIColor(red: 0.188, green: 0.220, blue: 0.286, alpha: 1)
+    public static let navy700 = UIColor(red: 0.141, green: 0.169, blue: 0.224, alpha: 1)
+    public static let navy900 = UIColor(red: 0.075, green: 0.090, blue: 0.129, alpha: 1)
+    public static let siteDeselected = UIColor(red: 0.757, green: 0.502, blue: 0.239, alpha: 0.10196078431372549)
+    public static let siteSelected = UIColor(red: 0.757, green: 0.502, blue: 0.239, alpha: 0.30196078431372547)
+    public static let sky100 = UIColor(red: 0.898, green: 0.922, blue: 0.929, alpha: 1)
+    public static let sky300 = UIColor(red: 0.765, green: 0.816, blue: 0.839, alpha: 1)
+    public static let sky500 = UIColor(red: 0.663, green: 0.737, blue: 0.769, alpha: 1)
+    public static let sky700 = UIColor(red: 0.596, green: 0.678, blue: 0.714, alpha: 1)
+    public static let sky900 = UIColor(red: 0.494, green: 0.592, blue: 0.659, alpha: 1)
+    public static let stone100 = UIColor(red: 0.969, green: 0.965, blue: 0.961, alpha: 1)
+    public static let stone300 = UIColor(red: 0.929, green: 0.929, blue: 0.918, alpha: 1)
+    public static let stone500 = UIColor(red: 0.859, green: 0.855, blue: 0.831, alpha: 1)
+    public static let stone700 = UIColor(red: 0.690, green: 0.686, blue: 0.655, alpha: 1)
+    public static let stone900 = UIColor(red: 0.231, green: 0.227, blue: 0.220, alpha: 1)
+    public static let teal100 = UIColor(red: 0.761, green: 0.855, blue: 0.863, alpha: 1)
+    public static let teal300 = UIColor(red: 0.439, green: 0.667, blue: 0.675, alpha: 1)
+    public static let teal500 = UIColor(red: 0.196, green: 0.522, blue: 0.537, alpha: 1)
+    public static let teal700 = UIColor(red: 0.149, green: 0.447, blue: 0.463, alpha: 1)
+    public static let teal900 = UIColor(red: 0.075, green: 0.333, blue: 0.349, alpha: 1)
 }
